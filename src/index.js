c7e99d368bd3910bb14aeda4bd6c34549bb5b3b1
Leandro Camargo <leandro.bcamargo@gmail.com>
2024-06-03 22:54:56 -0300

    Rename variables

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-const container = document.querySelector('#container');
+const outerContainer = document.querySelector('#container');
 
 function generateGrid() {
   for (let i = 0; i < 4; i++) {
     const innerContainer = document.createElement('div');
     innerContainer.classList.add('innerContainer'); 
     for (let j = 0; j < 4; j++) {
-      const div = document.createElement('div');
-      div.classList.add('gridUnit');
-      innerContainer.appendChild(div)
+      const gridUnit = document.createElement('div');
+      gridUnit.classList.add('gridUnit');
+      innerContainer.appendChild(gridUnit)
     }
-    container.appendChild(innerContainer);
+    outerContainer.appendChild(innerContainer);
   }
 }
 
