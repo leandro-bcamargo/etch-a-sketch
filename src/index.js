aa6cfc9058893246914d5ccfa6e6726994409aa1
Leandro Camargo <leandro.bcamargo@gmail.com>
2024-06-04 19:28:17 -0300

    Add dynamic setting of grid size

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const outerContainer = document.querySelector('#outerContainer');
 
-function generateGrid() {
-  for (let i = 0; i < 4; i++) {
+
+function generateGrid(gridSize) {
+  const outerContainerWidth = outerContainer.clientWidth;
+  const outerContainerHeight = outerContainer.clientHeight;
+  for (let i = 0; i < gridSize; i++) {
     const innerContainer = document.createElement('div');
     innerContainer.classList.add('innerContainer'); 
-    for (let j = 0; j < 4; j++) {
+    for (let j = 0; j < gridSize; j++) {
       const gridUnit = document.createElement('div');
+      gridUnit.style.width = `${outerContainerWidth/gridSize}px`;
+      gridUnit.style.height = `${outerContainerHeight/gridSize}px`;
       gridUnit.classList.add('gridUnit');
       gridUnit.addEventListener('mouseenter', (e) => colorGridUnit(e));
       innerContainer.appendChild(gridUnit)
@@ -19,4 +24,21 @@ function colorGridUnit(e) {
   gridUnit.classList.add('colorGridUnit');
 }
 
-generateGrid();
+function setGridSize() {
+  outerContainer.innerHTML = '';
+  const gridSize = prompt('What grid size would you like to set?');
+  if (gridSize > 100) throw new Error('Grid size is too large.')
+  generateGrid(gridSize);
+}
+
+const setGridSizeBtn = document.querySelector('#setGridSizeBtn');
+
+setGridSizeBtn.addEventListener('click', () => {
+  try {
+    setGridSize();
+  } catch (error) {
+    console.error(error.message);
+  }
+})
+
+window.onload = generateGrid(4);
